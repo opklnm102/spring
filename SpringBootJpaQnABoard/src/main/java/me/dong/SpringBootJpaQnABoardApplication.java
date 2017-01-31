a3f1e819c0e4c6afe38e7d16ab908eaf2b8e3401
DongHee Kim <opklnm102@gmail.com>
2017-01-31 14:35:56 +0900

    Inital commit: Spring Boot, JPA를 이용한 Q&A 게시판 만들기 study -> https://slipp.net/wiki/pages/viewpage.action?pageId=25529113
    ## 로컬 개발 환경 세팅
    * spring boot project
     * web
     * mustache - template engine
     * dev-tools - 개발을 도와주는 tool
    * hello world page
    * live reload chrome extension 설치
     * dev-tools와 연동
      * 잘 안먹는다 -> 참고: http://sbcoba.tistory.com/36

---
+++ SpringBootJpaQnABoard/src/main/java/me/dong/SpringBootJpaQnABoardApplication.java
@@ -0,0 +1,17 @@
+package me.dong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring-Boot, JPA로 질문/답변 게시판 구현 과정 Study
+ * https://slipp.net/wiki/pages/viewpage.action?pageId=25529113
+ */
+@SpringBootApplication
+public class SpringBootJpaQnABoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaQnABoardApplication.class, args);
+        System.out.println("Boot Run!!");
+    }
+}
