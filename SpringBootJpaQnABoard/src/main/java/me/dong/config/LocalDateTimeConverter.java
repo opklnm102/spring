9bba55966bf3fb2bb9c99de1e2c3bb19644ff114
DongHee Kim <opklnm102@gmail.com>
2017-02-06 20:30:37 +0900

    Spring Boot, JPA를 이용한 Q&A 게시판 만들기 study -> https://slipp.net/wiki/pages/viewpage.action?pageId=25529113 외부 톰켓 설정 추가

--- SpringBootJpaQnABoard/src/main/java/me/dong/config/LocalDateTimeConverter.java
+++ SpringBootJpaQnABoard/src/main/java/me/dong/config/LocalDateTimeConverter.java
@@ -1,4 +1,4 @@
-package me.dong;
+package me.dong.config;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
---
+++ SpringBootJpaQnABoard/src/main/java/me/dong/config/WebInitializer.java
@@ -0,0 +1,17 @@
+package me.dong.config;
+
+import me.dong.SpringBootJpaQnABoardApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by Dong on 2017-02-02.
+ * 외부 톰캣을 사용하기 위한 설정
+ */
+public class WebInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootJpaQnABoardApplication.class);
+    }
+}
